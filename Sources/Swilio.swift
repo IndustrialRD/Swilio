9c7cc72d0cc9844682e64dbeee6a90568e6a9a01
Khalid <mustkre8@gmail.com>
2016-07-15 18:09:02 -0700

    added SMS sending

--- Sources/Swilio.swift
+++ Sources/Swilio.swift
@@ -1,8 +1,16 @@
+import Foundation
+
 public struct Swilio {
     
     public static var accountSid:String?
     public static var authToken:String?
     
+    
+    static var session:URLSession {
+        let sessionConfig = URLSessionConfiguration.default
+        return URLSession(configuration: sessionConfig)
+    }
+
     public static var ready:Bool {
         return accountSid != nil && authToken != nil
     }
@@ -12,4 +20,41 @@ public struct Swilio {
         authToken = token
     }
     
+    public static func sendSMS(message body:String, to toPhone:String, from fromPhone:String, callback:(success:Bool)->Void) {
+        generateSendMessageTask(to: toPhone, from: fromPhone, body: body) { (data:Data?, response:URLResponse?, err:NSError?) in
+            if let d = data {
+                do{
+                    let json = try JSONSerialization.jsonObject(with: d, options: JSONSerialization.ReadingOptions.allowFragments) as! NSDictionary
+                    print(json)
+                    callback(success: true)
+                }catch{
+                    print(error)
+                    callback(success: false)
+                }
+            }
+        }.resume()
+    }
+    
+    static func generateSendMessageRequest() -> URLRequest {
+        var req = URLRequest(url:URL.messagesURL)
+        req.httpMethod = "POST"
+        req.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        return req
+    }
+    
+    static func generateSendMessageTask(to:String, from:String, body:String, callback:(Data?, URLResponse?, NSError?) -> Void )->URLSessionTask {
+        var req = generateSendMessageRequest()
+        let paramString = "From=\(from)&To=\(to)&Body=\(body)"
+        req.httpBody = paramString.data(using: String.Encoding.utf8)!
+        return session.dataTask(with: req, completionHandler: callback)
+    }
+}
+
+
+public extension URL {
+    static var messagesURL:URL {
+        let sid = Swilio.accountSid ?? "NO_SID"
+        let token = Swilio.authToken ?? "NO_TOKEN"
+        return URL(string: "https://\(sid):\(token)@api.twilio.com/2010-04-01/Accounts/\(sid)/Messages.json")!
+    }
 }
--- Tests/Swilio/SwilioTests.swift
+++ Tests/Swilio/SwilioTests.swift
@@ -1,3 +1,4 @@
+import Foundation
 import XCTest
 @testable import Swilio
 
@@ -20,11 +21,48 @@ class SwilioTests: XCTestCase {
         XCTAssert(Swilio.ready == false)
         
     }
+    
+    func testHasMethodForSendingSMS() {
+        XCTAssertNotNil(Swilio.sendSMS(message: "", to: "", from: "", callback: { (success) in
+        
+        }))
+    }
+    
+    func testHasMethodThatGeneratesRequestForSendingMessage() {
+        let fakeAcct = "fkeacct"
+        let fakeAuth = "#F4keT0K3n"
+        Swilio.configure(accountSid: fakeAcct, authToken: fakeAuth)
+        let req = Swilio.generateSendMessageRequest()
+        XCTAssert( req.url?.absoluteString == "https://\(fakeAcct):\(fakeAuth)@api.twilio.com/2010-04-01/Accounts/\(fakeAcct)/Messages.json")
+        XCTAssert(req.httpMethod == "POST")
+        XCTAssert(req.value(forHTTPHeaderField: "Content-Type") == "application/x-www-form-urlencoded")
+    }
 
+    
+    func testHasURKSession() {
+        XCTAssertNotNil(Swilio.session)
+    }
 
+    func testHasMethodThatGeneratesSendMessageTask() {
+        let fakeAcct = "fkeacct"
+        let fakeAuth = "#F4keT0K3n"
+        let fakeTo = "fkeacct"
+        let fakeFrom = "#F4keT0K3n"
+        let fakeBody = "fkeacct"
+        
+        Swilio.configure(accountSid: fakeAcct, authToken: fakeAuth)
+        let task = Swilio.generateSendMessageTask(to: fakeTo, from: fakeFrom, body: fakeBody) { (d:Data?, r:URLResponse?, e:NSError?) in}
+        
+        XCTAssert(String(data: (task.currentRequest?.httpBody!)!, encoding: String.Encoding.utf8) == "From=\(fakeFrom)&To=\(fakeTo)&Body=\(fakeBody)")
+    }
+    
+    
     static var allTests : [(String, (SwilioTests) -> () throws -> Void)] {
         return [
             ("testHasOptionForConfiguration", testHasOptionForConfiguration),
+            ("testHasReadyProperty", testHasReadyProperty),
+            ("testHasMethodForSendingSMS", testHasMethodForSendingSMS),
+            ("testHasMethodThatGeneratesRequestForSendingMessage", testHasMethodThatGeneratesRequestForSendingMessage)
         ]
     }
 }
